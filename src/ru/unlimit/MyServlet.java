8ab996cfb5829c67d5cf119e4042f3df0e7ba558
loudbyte <davebottomhell@gmail.com>
2020-01-28 03:42:37 +0600

    Creating servlet. Using form, doGet, doPost, request, response

---
+++ src/ru/unlimit/MyServlet.java
@@ -0,0 +1,59 @@
+package ru.unlimit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("firstName");
+		String surname = request.getParameter("secondName");
+		System.out.println(name);
+		System.out.println(surname);
+		
+		
+		System.out.println("Info");
+		
+//		response.setContentType("text/html");
+//		response.setCharacterEncoding("UTF-8");
+//		
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Hello from MyServlet.GET "+name+" "+surname+"</h3>");
+		out.close();
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("firstName");
+		String surname = request.getParameter("secondName");
+		System.out.println(name);
+		System.out.println(surname);
+		
+		
+		System.out.println("Info");
+		
+//		response.setContentType("text/html");
+//		response.setCharacterEncoding("UTF-8");
+//		
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Hello from MyServlet.POST "+name+" "+surname+"</h3>");
+		out.close();
+	}
+
+}
